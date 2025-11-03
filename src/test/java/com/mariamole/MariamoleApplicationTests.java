fe53332693c384a6e965a1985ed4c60a830af862
Ethelynaparecida <ethelynaparecida@gmail.com>
2025-11-02 23:07:07 -0300

    fix

---
+++ src/test/java/com/mariamole/MariamoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariamole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MariamoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
